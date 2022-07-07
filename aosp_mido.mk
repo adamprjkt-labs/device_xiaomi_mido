1d63f35d62e027b31d414530c68a9089750eb9bd
NRanjan-17 <nalinishranjan05@gmail.com>
2022-07-07 18:45:25 +0530

    mido: Update build fp and desc to Pixel 6 Pro (raven) July release
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -42,7 +42,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user $(PLATFORM_VERSION) $(BUILD_ID) $(shell date -u +%s) release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003.A1 8672226 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date -u +%H%M):user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
