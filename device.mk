cc7141644c58a5856922bf69ef8bc24a4327bdb4
NRanjan-17 <nalinishranjan05@gmail.com>
2022-07-19 05:55:20 +0530

    mido: add NOTHING
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 $(call inherit-product, vendor/xiaomi/mido/google/google.mk)
+$(call inherit-product, vendor/nothing/config.mk)
 
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_SUFFIX := _64
